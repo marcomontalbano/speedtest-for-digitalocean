937f5f0196d2acc1445d125c70769110815bdaf4
Marco Montalbano <marcomontalbano.work@gmail.com>
2018-04-16 22:17:37 +0200

    bump version: 1.0.0

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import App from './app/components/App';
 //import registerServiceWorker from './registerServiceWorker';
 
+import packageJson from '../package.json';
+
 import '../node_modules/uikit/dist/css/uikit.css';
 import './index.css';
 
 ReactDOM.render(
     <App />,
     document.getElementById('root')
+);
+
+ReactDOM.render(
+    (
+        <div>
+            v {packageJson.version}
+        </div>
+    ),
+    document.getElementById('version')
 );
 
 //registerServiceWorker();
