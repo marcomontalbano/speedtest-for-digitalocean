894ec368b48336865f98009f1ffab73fcdaf50b2
Marco Montalbano <marcomontalbano.work@gmail.com>
2020-06-09 09:53:51 +0200

    Update npm dependencies

--- src/app/components/Spinner.js
+++ src/app/components/Spinner.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './Spinner.css';
 
-class Spinner extends Component {
-
-    render() {
-        return (
-            <div className="sk-three-bounce">
-                <div className="sk-child sk-bounce1"></div>
-                <div className="sk-child sk-bounce2"></div>
-                <div className="sk-child sk-bounce2"></div>
-            </div>
-        );
-    }
-
-}
+const Spinner = () => (
+    <div className="sk-three-bounce">
+        <div className="sk-child sk-bounce1"></div>
+        <div className="sk-child sk-bounce2"></div>
+        <div className="sk-child sk-bounce2"></div>
+    </div>
+)
 
 export default Spinner;
