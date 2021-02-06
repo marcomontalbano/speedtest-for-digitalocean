afd8c1cca723cbbf731169e8d8c0d7ac67e082b8
Marco Montalbano <marcomontalbano.work@gmail.com>
2021-02-07 00:11:46 +0100

    Update jsdelivr link

--- bookmark.js
+++ bookmark.js
@@ -1,9 +1,10 @@
 var s = document.createElement('script');
-s.src = 'https://cdn.jsdelivr.net/gh/marcomontalbano/speedtest-for-digitalocean@gh-pages/static/js/main.js';
+s.src = 'https://cdn.jsdelivr.net/gh/marcomontalbano/speedtest-for-digitalocean@latest/static/js/main.js';
 
 var l = document.createElement('link');
-l.href = 'https://cdn.jsdelivr.net/gh/marcomontalbano/speedtest-for-digitalocean@gh-pages/static/css/main.css';
+l.href = 'https://cdn.jsdelivr.net/gh/marcomontalbano/speedtest-for-digitalocean@latest/static/css/main.css';
 l.rel = 'stylesheet';
 
 document.body.setAttribute('id', 'st4do-root');
+document.head.innerHTML = '';
 document.head.append(s, l);
