a62c585b094bfafd42bd230a489df26533dccee8
Marco Montalbano <marcomontalbano.work@gmail.com>
2022-01-18 20:53:17 +0100

    Update bookmark link avoiding loading app multiple times

--- bookmark.js
+++ bookmark.js
@@ -1,12 +1,20 @@
-var s = document.createElement('script');
-s.src = 'https://cdn.jsdelivr.net/gh/marcomontalbano/speedtest-for-digitalocean@latest/static/js/main.js';
+if (!/^speedtest\-.*\.digitalocean\.com$/.test(location.hostname)) {
+  location.href = 'http://speedtest-do.marcomontalbano.com'
+}
 
-var l = document.createElement('link');
-l.href = 'https://cdn.jsdelivr.net/gh/marcomontalbano/speedtest-for-digitalocean@latest/static/css/main.css';
-l.rel = 'stylesheet';
-
-document.head.innerHTML = '';
-document.body.innerHTML = '';
-
-document.body.setAttribute('id', 'st4do-root');
-document.head.append(s, l);
+if (document.body.getAttribute('id') !== 'st4do-root') {
+  var r = Math.random()
+  
+  var s = document.createElement('script');
+  s.src = 'https://cdn.jsdelivr.net/gh/marcomontalbano/speedtest-for-digitalocean@latest/static/js/main.js?_=' + r;
+  
+  var l = document.createElement('link');
+  l.href = 'https://cdn.jsdelivr.net/gh/marcomontalbano/speedtest-for-digitalocean@latest/static/css/main.css?_=' + r;
+  l.rel = 'stylesheet';
+  
+  document.head.innerHTML = '';
+  document.body.innerHTML = '';
+  
+  document.body.setAttribute('id', 'st4do-root');
+  document.head.append(s, l);
+}
